41cbfe260a9d3d3f0789941248e9c577ddf1377d
Nohinora <nohinora@proton.me>
2024-02-23 19:53:30 +0100

    Corrected typo

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'httpps://assioloviviconero.github.io',
+  site: 'https://assioloviviconero.github.io',
 });
